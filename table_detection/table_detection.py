3342154518e3710a8151adfa5ca4c873e334fe12
Luan Pham <luan@cinnamon.is>
2022-07-18 14:05:35 +0700

    Add init code for table_detection.py

---
+++ table_detection/table_detection.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+# import cv2
+
+prj_root = Path(__file__).parent.parent.resolve()
+
+def main():
+    input_image = cv2.imread(os.path.join(prj_root, "sample.jpg"))
+    # TODO: code here
+
+    return [{
+        "name": "table",
+        "xmin": 100,
+        "ymin": 100,
+        "xmax": 200,
+        "ymax": 200,
+    }]
+
+if __name__ == "__main__":
+    main()
