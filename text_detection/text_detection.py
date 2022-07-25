0a6b8ac7337650f560ede1415cdbc7d1c5ad48c7
ankhanh-vni <84317220+ankhanh-vni@users.noreply.github.com>
2022-07-25 23:50:10 +0700

    dev text detection module

--- text_detection/text_detection.py
+++ text_detection/text_detection.py
@@ -1,40 +1,20 @@
-import os
-from pathlib import Path
+import torch, torchvision
+import mmcv
+import mmdet
+import mmocr
 
-import easyocr
-
-prj_root = Path(__file__).parent.parent.resolve()
+from mmocr.utils.ocr import MMOCR
 
 
-class TextDetector:
-    instance = None
+def main():
+    img_file =""
+    output_file = ""
+    model_name = ""
+    
+    mmocr = MMOCR(det=model_name, recog=None)
+    output = mmocr.readtext(img_file, print_result=True, output=output_file, export_format = "json")
+    
+    return output
 
-    def __init__(self, weights_path=None):
-        self.reader = easyocr.Reader(["en"])
-
-        if weights_path is None:
-            weights_path = os.path.join(os.path.dirname(__file__), "weight.pth")
-            assert os.path.exists(weights_path), weights_path
-
-    def process(self, image):
-        outputs = []
-        output = self.reader.readtext(image)
-
-        for id, row in enumerate(output):
-            box = {}
-            box["name"] = str(id)
-            box["xmin"] = row[0][0][0]
-            box["xmax"] = row[0][2][0]
-            box["ymin"] = row[0][0][1]
-            box["ymax"] = row[0][2][1]
-
-            outputs.append(box)
-
-        return outputs
-
-    @classmethod
-    def get_unique_instance(cls):
-        if cls.instance is None:
-            # initialize instance
-            cls.instance = cls()
-        return cls.instance
+if __name__ == '__main__':
+    main()
