06f4f763538dc355f2dc29d17aee396c823386f9
Luan Pham <phamquiluan@gmail.com>
2022-08-15 12:01:20 +0000

    Add TableRecognizer

--- table_recognition/table_recognition.py
+++ table_recognition/table_recognition.py
@@ -4,6 +4,49 @@ from pathlib import Path
 
 prj_root = Path(__file__).parent.parent.resolve()
 
+class TableRecognizer:
+    instance = None
+
+    def __init__(self, weights_path = None):
+        # init your model here
+        if weights_path is None:
+            weights_path = os.path.join(
+                os.path.dirname(__file__),
+                "weight.pth"
+            ) 
+            assert os.path.exists(weights_path), weights_path
+        
+        # TODO: change this
+        self.model = lambda x: [{
+            "name": "table",
+            "xmin": 100,
+            "ymin": 100,
+            "xmax": 200,
+            "ymax": 200,
+            "cell_list": [
+                {
+                    "name": "cell",
+                    "xmin": 100,
+                    "ymin": 100,
+                    "xmax": 200,
+                    "ymax": 200
+                }
+            ]
+        }]
+
+    def process(self, image, table_list : list):
+        # TODO: do something here 
+        output = self.model(image)
+        return output
+
+    @classmethod
+    def get_unique_instance(cls):
+        if cls.instance is None:
+            # initialize instance
+            cls.instance = cls()
+        return cls.instance
+
+
 def main():
     input_image = cv2.imread(os.path.join(prj_root, "sample.jpg"))
     input_table_list = [{
