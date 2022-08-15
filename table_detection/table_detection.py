568c42e57b07c75a1bb74165ce6a062d8de010ff
Luan Pham <phamquiluan@gmail.com>
2022-08-15 11:50:18 +0000

    Add TableDetector

--- table_detection/table_detection.py
+++ table_detection/table_detection.py
@@ -1,8 +1,35 @@
 import os
 from pathlib import Path
-# import cv2
+import cv2
 
 prj_root = Path(__file__).parent.parent.resolve()
+
+class TableDetector:
+    instance = None
+
+    def __init__(self, weights_path = None):
+        # init your model here
+        if weights_path is None:
+            weights_path = os.path.join(
+                os.path.dirname(__file__),
+                "weight.pth"
+            ) 
+            assert os.path.exists(weights_path), weights_path
+        
+        # TODO: change this
+        self.model = lambda x: [100, 100, 200, 200]
+
+    def process(self, image):
+        output = self.model(image)
+        return output
+
+    @classmethod
+    def get_unique_instance(cls):
+        if cls.instance is None:
+            # initialize instance
+            cls.instance = cls()
+        return cls.instance
+
 
 def main():
     input_image = cv2.imread(os.path.join(prj_root, "sample.jpg"))
