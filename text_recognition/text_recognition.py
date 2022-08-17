e17e0060cdc10c3245f6e5c766e9e50b27ca2f14
Luan Pham <luan@cinnamon.is>
2022-08-18 02:03:35 +0700

    dockerize text recognition module

--- text_recognition/text_recognition.py
+++ text_recognition/text_recognition.py
@@ -1,3 +1,38 @@
+import os
+
+class TextRecognizer:
+    instance = None
+
+    def __init__(self, weights_path = None):
+        # init your model here
+        if weights_path is None:
+            weights_path = os.path.join(
+                os.path.dirname(__file__),
+                "weight.pth"
+            ) 
+            assert os.path.exists(weights_path), weights_path
+        
+        # TODO: change this
+        self.model = lambda x, y: [{
+            "name": "text",
+            "xmin": 100,
+            "ymin": 100,
+            "xmax": 200,
+            "ymax": 200,
+            "text": "hello world!"
+        }]
+
+    def process(self, image, text_list : list):
+        output = self.model(image, text_list)
+        return output
+
+    @classmethod
+    def get_unique_instance(cls):
+        if cls.instance is None:
+            # initialize instance
+            cls.instance = cls()
+        return cls.instance
+
 
 
 class OCRModel:
