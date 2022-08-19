188fb6bb42e50a29fb707a77c6af6f9607343019
Luan Pham <luan@cinnamon.is>
2022-08-19 16:07:05 +0700

    wip

---
+++ text_detection/tmp.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import cv2
+import numpy as np
+import glob
+
+for image_path in glob.glob("../data/images/*.jpg"):
+    image_name = os.path.basename(image_path)
+
+    # predict
+    with open(image_path, "rb") as ref:
+        output = requests.request(
+            method="POST"
+            url="http://127.0.0.1:8080/predictions/dbnet",
+            headers={},
+            data={},
+            files=
+            data={
+            "data": ref
+        })
+        print(output)
+
+        print(output.text)
+
+
+    # visualize
+    # image = cv2.imread(image_path)
+
+
+
+
