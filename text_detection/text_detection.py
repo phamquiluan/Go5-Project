87598cebfe4dc09156ed156fe2f6c88f2e309ac7
Luan Pham <luan@cinnamon.is>
2022-08-31 22:25:52 +0700

    Clean text_detection

--- text_detection/text_detection.py
+++ text_detection/text_detection.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 import easyocr
-import cv2
 
 prj_root = Path(__file__).parent.parent.resolve()
 
@@ -11,39 +10,23 @@ class TextDetector:
     instance = None
 
     def __init__(self, weights_path=None):
-        # init your model here
-
-        self.reader = easyocr.Reader(['en'])
+        self.reader = easyocr.Reader(["en"])
 
         if weights_path is None:
             weights_path = os.path.join(os.path.dirname(__file__), "weight.pth")
             assert os.path.exists(weights_path), weights_path
 
-        # TODO: change this
-        # self.model = lambda x: [
-        #     {
-        #         "name": "text",
-        #         "xmin": 100,
-        #         "ymin": 100,
-        #         "xmax": 200,
-        #         "ymax": 200,
-        #     }
-        # ]
-
-
-
     def process(self, image):
         outputs = []
-
         output = self.reader.readtext(image)
-        
+
         for id, row in enumerate(output):
             box = {}
-            box['name'] = str(id)
-            box['xmin'] = row[0][0][0]
-            box['xmax'] = row[0][2][0]
-            box['ymin'] = row[0][0][1]
-            box['ymax'] = row[0][2][1]
+            box["name"] = str(id)
+            box["xmin"] = row[0][0][0]
+            box["xmax"] = row[0][2][0]
+            box["ymin"] = row[0][0][1]
+            box["ymax"] = row[0][2][1]
 
             outputs.append(box)
 
@@ -55,23 +38,3 @@ class TextDetector:
             # initialize instance
             cls.instance = cls()
         return cls.instance
-
-
-def main():
-    cv2.imread(os.path.join(prj_root, "sample.jpg"))
-
-    # TODO: code here
-
-    return [
-        {
-            "name": "table",
-            "xmin": 100,
-            "ymin": 100,
-            "xmax": 200,
-            "ymax": 200,
-        }
-    ]
-
-
-if __name__ == "__main__":
-    main()
